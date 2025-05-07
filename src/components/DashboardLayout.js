d2853324fffc2814cd70f20963483efbe2a01967
razabhatti546 <ali.raza848429@gmail.com>
2025-05-07 15:15:35 +0500

    new changes

--- src/components/DashboardLayout.js
+++ src/components/DashboardLayout.js
@@ -37,7 +37,7 @@ export default function DashboardLayout({ children }) {
   }, [isSidebarOpen]);
 
   return (
-    <div className="min-h-screen bg-gray-900">
+    <div className="min-h-screen bg-black">
       <Header isOpen={isSidebarOpen} />
       <Sidebar isOpen={isSidebarOpen} toggleSidebar={toggleSidebar} />
 
